35f952438bf0005d937f839483d15d9e5442affe
Alwin <nedrum@gmail.com>
2021-09-19 11:49:09 +0200

    master setup

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Jumbotron from '../components/jumbotron'
+
+export default function Home() {
+
+    return (
+
+        <Jumbotron.Container>
+            <Jumbotron.Title>NUTFLIX</Jumbotron.Title>
+            <Jumbotron.SubTitle>Filmography of the world of nuts</Jumbotron.SubTitle>
+        </Jumbotron.Container>
+
+
+
+    )
+
+}
