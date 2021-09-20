703b11cda431311138e1731f373d34fa2fc5df0c
Alwin <nedrum@gmail.com>
2021-09-20 14:03:13 +0200

    Styled the footer

--- src/containers/footerContainer.js
+++ src/containers/footerContainer.js
@@ -11,23 +11,36 @@ export function FooterContainer() {
                 <Footer.Column>
                     <Footer.Link href="#">FAQs</Footer.Link>
                     <Footer.Link href="#">Investor Relations</Footer.Link>
-                    <Footer.Link href="#"></Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
+                    <Footer.Link href="#">Ways to Watch</Footer.Link>
+                    <Footer.Link href="#">Corporate Information</Footer.Link>
+                    <Footer.Link href="#">OrignalNuts</Footer.Link>   
                 </Footer.Column>
+
                 <Footer.Column>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
-                    <Footer.Link href="#">FAQs</Footer.Link>
+                    <Footer.Link href="#">Help Center</Footer.Link>
+                    <Footer.Link href="#">Jobs</Footer.Link>
+                    <Footer.Link href="#">Terms of Use</Footer.Link>
+                    <Footer.Link href="#">Contact Us</Footer.Link>
                 </Footer.Column>
 
-
-
+                <Footer.Column> 
+                    <Footer.Link href="#">Account</Footer.Link>
+                    <Footer.Link href="#">Redeem gift cards</Footer.Link>
+                    <Footer.Link href="#">Privacy</Footer.Link>
+                    <Footer.Link href="#">Speed test</Footer.Link>   
+                </Footer.Column>
+              
+                <Footer.Column> 
+                    <Footer.Link href="#">Media Center</Footer.Link>
+                    <Footer.Link href="#">Buy gift cards</Footer.Link>
+                    <Footer.Link href="#">Cookie Preferences</Footer.Link>
+                    <Footer.Link href="#">Legal Notice</Footer.Link>   
+                </Footer.Column>
             </Footer.Row>
+            <Footer.Break/>
+
+        <Footer.Text>NutFlix Global</Footer.Text>
+
         </Footer>
     )
 }
