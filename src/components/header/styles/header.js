af83c9acea5a9954ef96d03779951e718e7c34fe
Alwin <nedrum@gmail.com>
2021-09-22 10:07:24 +0200

    Adding  Header Component

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Background = styled.section`
+    display: flex; 
+    flex-direction: column;
+
+`;
+
+export const Logo = styled.img`
+    height: 32px;
+    width: 108px;
+    margin-right: 40px;
+    
+    
+    @media (min-width: 1149px) {
+        height: 45px;
+        width: 167px;
+
+    }
+`;
+
+export const Container = styled.div`
+    
+
+
+`;
+
+export const Link = styled.p``;
+
+export const ButtonLink = styled.button``;
+
+
+
