00e5ef09c1f2b5764129642d47e2f9e3dc9482d5
Alwin <nedrum@gmail.com>
2021-09-22 10:07:49 +0200

    Adding Header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { JumbotronContainer } from '../containers/jumbotronContainer';
 import { FooterContainer } from '../containers/footerContainer';
 import { FaqsContainer } from '../containers/faqsContainer';
+import { HeaderContainer } from '../containers/HeaderContainer';
 
 
 export default function Home() {
@@ -9,6 +10,10 @@ export default function Home() {
     return (
 
        <>
+    <HeaderContainer>
+        <p>HELLOFLIX</p>
+    </HeaderContainer>
+
         <JumbotronContainer/>
         <FaqsContainer />
         <FooterContainer />
