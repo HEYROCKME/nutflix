2b224cafe0ea121ff7e35030d2fe979ed2c4e39f
Alwin <nedrum@gmail.com>
2021-10-01 15:30:06 +0200

    Adding Feature component in browser page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link as ReachRouterLink } from 'react-router-dom'
-import {Background, Container, Logo, ButtonLink } from './styles/header'
+import { Feature } from "..";
+import {Background, Container, Logo, ButtonLink, Group, Text, Link, Feature, CallOut, PlayButton } from './styles/header'
 
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -23,7 +24,34 @@ Header.Logo = function HeaderLogo({to, ...restProps}) {
     )
 }
 
+
+Header.Group = function HeaderGroup({children, ...restProps}) {
+    return <Group {...restProps}>{children}</Group>
+}
+
+
+
 Header.ButtonLink = function HeaderButtonLink({children, ...restProps}) {
 
     return <ButtonLink {...restProps}>{children}</ButtonLink>
+}
+Header.Text = function HeaderText({children, ...restProps}) {
+
+    return <Text {...restProps}>{children}</Text>
+}
+Header.Link = function HeaderLink({children, ...restProps}) {
+
+    return <Link {...restProps}>{children}</Link>
+}
+
+Header.Feature = function HeaderFeature({children, ...restProps}) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}) {
+    return <CallOut {...restProps}>{children}</CallOut>
+}
+
+Header.PlayButton = function HeaderPlayButton({children, ...restProps}) {
+    return <PlayButton {...restProps}>{children}</PlayButton>
 }
